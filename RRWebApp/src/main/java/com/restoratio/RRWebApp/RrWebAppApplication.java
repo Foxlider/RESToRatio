d514f747baa7dfac9beac032a3a07098df3a5e13
Keelah Atom <al.loncham@gmail.com>
2023-05-24 09:21:16 +0200

    printempsbotte

---
+++ RRWebApp/src/main/java/com/restoratio/RRWebApp/RrWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.restoratio.RRWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrWebAppApplication.class, args);
+	}
+
+}
